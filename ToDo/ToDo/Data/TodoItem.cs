ea7aceef2373ab3d83c933d2f9c7e9bb824c8d4b
Petar Tomasevic <petar.tomasevic@ri-ing.net>
2021-06-22 15:58:55 +0200

    initial commit

---
+++ ToDo/ToDo/Data/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo.Data
+{
+    public class TodoItem
+    {
+        public TodoItem(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; set; }
+        public bool IsComplete { get; set; } = false;
+    }
